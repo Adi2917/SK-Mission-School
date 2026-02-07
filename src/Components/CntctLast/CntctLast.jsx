3f2c9623140dcfeeb5e5b1ccae10127a9155aab5
Adi2917 <samratadi4321@gmail.com>
2026-02-07 20:34:41 +0530

    map location changed

--- src/Components/CntctLast/CntctLast.jsx
+++ src/Components/CntctLast/CntctLast.jsx
@@ -46,7 +46,7 @@ const CntctLast = () => {
         {/* MAP */}
         <div className="map-box">
           <a
-            href="https://www.google.com/maps?q=25.7813,87.4750"
+            href="https://www.google.com/maps?q=25.770966,87.483953"
             target="_blank"
             rel="noreferrer"
           >
@@ -57,7 +57,7 @@ const CntctLast = () => {
 
             <iframe
               title="School Map"
-              src="https://maps.google.com/maps?q=25.7813,87.4750&z=15&output=embed"
+              src="https://maps.google.com/maps?q=25.770966,87.483953&z=15&output=embed"
               loading="lazy"
             ></iframe>
           </a>
